843bcd4cf6e4743cf12a39ba82bd7748652d4f28
Reggy Pramudya Sulaeman <91525358+Reggypramudyas@users.noreply.github.com>
2021-10-22 18:59:24 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,59 @@
+class Main {
+    public static void main(String[] args) {
+        Hero hero = new Hero("Reggy" , 20);
+
+        hero.player();
+
+        hero.walk();
+        hero.walk();
+        hero.walk();
+        hero.walk();
+        hero.walk();
+        hero.walk();
+        hero.walk();
+        hero.walk();
+        hero.walk();
+        hero.walk();
+        hero.walk();
+        hero.walk();
+        hero.walk();
+        hero.walk();
+        hero.walk();
+        hero.walk();
+        hero.walk();
+        hero.walk();
+        hero.walk();
+        hero.walk();
+        hero.walk();
+        hero.run();
+        hero.run();
+        hero.run();
+        hero.run();
+        hero.run();
+        hero.eat();
+        hero.eat();
+        hero.eat();
+        hero.drink();
+        hero.drink();
+        hero.walk();
+        hero.walk();
+        hero.walk();
+        hero.walk();
+        hero.walk();
+        hero.jump();
+        hero.jump();
+        hero.sit();
+        hero.attack();
+        hero.attack();
+        hero.attack();
+
+
+
+        // Group 1_Siang : walk 20x, run 5x, eat 3x, drink 3x, walk 5x, jump 2x, sit 1x, attack 3x
+
+        hero.player();
+
+
+
+    }
+}
